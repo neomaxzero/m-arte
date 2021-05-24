7b698d63d861b6dfb1a6d4011516f4dd2d640257
Max <maxicespedes7@hotmail.com>
2021-05-24 19:40:31 +0200

    [003] - Show title and add art render template string

---
+++ src/client/components/art.js
@@ -0,0 +1,9 @@
+const art = (data) => {
+  return `
+    <div class="art">
+      ${data.title}
+    </div>
+  `;
+};
+
+export default art;
--- src/client/components/artgrid.js
+++ src/client/components/artgrid.js
@@ -1,15 +1,22 @@
 import { getArtworks } from '../repository/artwork';
+import renderArt from './art';
 import { componentFactory } from './core';
 
 const artgrid = (el) => {
   async function getData() {
     setLoader(true);
     const { error, data, errorMsg } = await getArtworks();
-
+    console.log(data);
     if (error) {
       showError(errorMsg);
     }
+
     setLoader(false);
+
+    const cards = data.data.map(renderArt);
+    console.log(cards);
+
+    el.innerHTML = cards.join('');
   }
 
   function setLoader(show) {
